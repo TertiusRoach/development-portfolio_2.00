bdf7a702c0aec1aaa846534d254ef83f8e0b4322
Tertius Roach <tertius.roach@gmail.com>
2023-12-31 22:39:13 +0200

    Added function to delete types folder

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp');
 
+const clean = require('gulp-clean');
 const { src, dest } = require('gulp');
 const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const htmlmin = require('gulp-htmlmin');
+const gap = require('gulp-append-prepend');
+const uglifycss = require('gulp-uglifycss');
 const typescript = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const replace = require('gulp-string-replace');
+const deletefile = require('gulp-delete-file');
+const sass = require('gulp-sass')(require('sass'));
+const removeHtmlComments = require('gulp-remove-html-comments');
 
 gulp.task('copyIndex', async () => {
   let pageName = 'index';
@@ -12,34 +22,37 @@ gulp.task('copyIndex', async () => {
 });
 
 const compileTypeScript = () => {
-  //--|▼| Build reference map for compiler |▼|--//
+  //--|🠋| Build reference map for compiler |🠋|--//
   const reference = () => {
-    //--|▼| Reference 'tsconfig.json' |▼|--//
+    //--|🠋| Reference 'tsconfig.json' |🠋|--//
     const typeScriptProject = typescript.createProject('tsconfig.json');
-    //--|▼| Get TypeScript source code |▼|--//
+    //--|🠋| Get TypeScript source code |🠋|--//
     const sourceCode = typeScriptProject.src();
-    //--|▼| Initialize TypeScript map for export |▼|--//
+    //--|🠋| Initialize TypeScript map for export |🠋|--//
     const initializeSourcemaps = sourcemaps.init();
-    //--|▼| Give source files its JavaScript identity |▼|--//
+    //--|🠋| Give source files its JavaScript identity |🠋|--//
     const IdentityMap = sourcemaps.identityMap();
-    //--|▼| Return code for compiling |▼|--//
+    //--|🠋| Return code for compiling |🠋|--//
     return sourceCode.pipe(initializeSourcemaps).pipe(IdentityMap).pipe(typeScriptProject());
   };
 
-  //--|▼| Map out TypeScript to dist folder |▼|--//
+  //--|🠋| Map out TypeScript to dist folder |🠋|--//
   let srcUrlMapper = (file) => {
     let distFolder = gulp.dest('dist/');
     return distFolder + file.relative.toString().split('\\').join('/') + '.map';
   };
 
-  //--|▼| Compile TypeScript |▼|--//
+  //--|🠋| Compile TypeScript |🠋|--//
   let compileTypes = () => {
     let typesFolder = gulp.dest('types/');
     let typeScriptCompiled = reference();
 
-    typeScriptCompiled.dts.pipe(typesFolder).on('error', function (err) {
-      console.log('Gulp says: ' + err.message);
-    });
+    typeScriptCompiled.dts
+      //--|🠋| Description Here |🠋|--//
+      .pipe(typesFolder)
+      .on('error', function (err) {
+        console.log('Gulp says: ' + err.message);
+      });
 
     typeScriptCompiled.js
       .pipe(
@@ -54,8 +67,12 @@ const compileTypeScript = () => {
       )
       .pipe(dest('dist/'));
   };
-
   compileTypes();
-};
 
-//-------------------------------------------------//
+  let remove = () => {
+    console.log('|🠊 Deleting types folder');
+    //--|🠋| Delete types folder |🠋|--//
+    return gulp.src('types', { read: false }).pipe(clean());
+  };
+  setTimeout(remove, 7500);
+};
