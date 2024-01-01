f9d406322eb329d12a82117ab80f24922829d6fd
Tertius Roach <tertius.roach@gmail.com>
2024-01-01 11:04:03 +0200

    Made this utility more broad: Get is now a keyword.

---
+++ src/front-end/utilities/Get.ts
@@ -0,0 +1,10 @@
+export namespace Get {
+  export class page {
+    constructor(blockName: String) {
+      console.log(blockName);
+
+      console.log(`🠊 Get.page(${blockName}); 🠈`);
+      //--|🠊| console.log('🠊 Get.page('blockName'); 🠈'); |🠈|--//
+    }
+  }
+}
--- src/front-end/utilities/GetSection.ts
+++
@@ -1,8 +0,0 @@
-export namespace GetSection {
-  export class forSection {
-    constructor(blockName: String) {
-      console.log(blockName);
-      // console.log(`🠊 GetSection.js Loaded 🠈`);
-    }
-  }
-}
