2237cf7baeae494420109bb29274e47960b41485
Tertius Roach <tertius.roach@gmail.com>
2024-01-23 19:05:46 +0200

    Firefox is 10 steps ahead

--- src/front-end/pages/resume/C-header/default-header/default-header.ts
+++ src/front-end/pages/resume/C-header/default-header/default-header.ts
@@ -1,5 +1,10 @@
 export namespace DefaultHeader {
   export function events() {
+    $('.header-navigation a').on('click', function (event) {
+      $('.header-navigation a').removeClass('active');
+      $(event.currentTarget).addClass('active');
+    });
+
     console.log('default-header.js Detected!');
   }
 }
