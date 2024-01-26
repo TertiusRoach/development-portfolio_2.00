7663c49ec2ead997abb446c128512ef727395b93
Tertius Roach <tertius.roach@gmail.com>
2024-01-26 15:01:22 +0200

    Header auto-marks and logo added

--- src/front-end/pages/resume/C-header/default-header/default-header.ts
+++ src/front-end/pages/resume/C-header/default-header/default-header.ts
@@ -1,6 +1,7 @@
 export namespace DefaultHeader {
   export function events() {
     $('.header-navigation a').on('click', function (event) {
+      //--|🠋| Mark Header as Active |🠋|--//
       $('.header-navigation a').removeClass('active');
       $(event.currentTarget).addClass('active');
     });
--- src/front-end/pages/resume/G-main/default-main/default-main.ts
+++ src/front-end/pages/resume/G-main/default-main/default-main.ts
@@ -1,5 +1,15 @@
 export namespace DefaultMain {
   export function events() {
+    $('.default-main section').on('mouseover', function (event) {
+      //--|🠋| Mark Header as Active |🠋|--//
+      let headerElement = document.querySelector(`.header-navigation #header-${event.currentTarget.id.split('-')[1]}`);
+      switch (headerElement.classList[0]) {
+        case undefined:
+          $('.header-navigation a').removeClass('active');
+          $(headerElement).addClass('active');
+          break;
+      }
+    });
     console.log('default-main.js Detected!');
   }
 }
