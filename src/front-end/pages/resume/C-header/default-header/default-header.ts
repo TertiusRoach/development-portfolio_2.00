cb08ee61bdb6cef473de6ebff769be6297702739
Tertius Roach <tertius.roach@gmail.com>
2024-02-06 23:05:48 +0200

    Fixed Snag Overlays

--- src/front-end/pages/resume/C-header/default-header/default-header.ts
+++ src/front-end/pages/resume/C-header/default-header/default-header.ts
@@ -2,28 +2,59 @@ export namespace DefaultHeader {
   export function events() {
     //--🠋 Desktop (Landscape) 🠋--//
     $('#header-navigation a').on('click', (event) => {
-      //--|🠋| Remove all 'active' classes |🠋|--//
-      $('#header-navigation a').removeClass('active');
-      //--|🠋| Highlight Clicked Header |🠋|--//
-      $(event.currentTarget).addClass('active');
+      selectHeader(event);
     });
 
     //--🠋 Mobile (Portrait) 🠋--//
     $('#rightbar-hitbox').on('click', (event) => {
-      if (event.currentTarget.className === '') {
-        $('#resume-rightbar').css('display', 'flex');
-
-        setTimeout(() => {
-          //--|🠋| Highlight Header |🠋|--//
-          $('#rightbar-hitbox').addClass('active');
-          $('#header-portrait').addClass('active');
-
-          //--|🠋| Show Rightbar |🠋|--//
-          $('#rightbar-portrait').removeClass('hidden');
-          $('#rightbar-portrait').addClass('visible');
-        }, 125);
+      switch (event.currentTarget.className) {
+        case 'active':
+          downplayHeader();
+          hideRightbar();
+          break;
+        case '':
+          showRightbar();
+          highlightHeader();
+          break;
       }
     });
+
     console.log('default-header.js Detected!');
+  }
+
+  //--|🠋| default-header |🠋|--//
+  function selectHeader(event: any) {
+    //--|🠋| Remove all 'active' classes |🠋|--//
+    $('#header-navigation a').removeClass('active');
+    //--|🠋| Highlight Clicked Header |🠋|--//
+    $(event.currentTarget).addClass('active');
+  }
+  function highlightHeader() {
+    //--|🠋| Highlight Header |🠋|--//
+    setTimeout(() => {
+      $('#rightbar-hitbox').addClass('active');
+      $('#header-portrait').addClass('active');
+    }, 125);
+  }
+  function downplayHeader() {
+    //--|🠋| Downplay Header |🠋|--//
+    $('#rightbar-hitbox').removeClass('active');
+    $('#header-portrait').removeClass('active');
+  }
+  function showRightbar() {
+    //--|🠋| Show Rightbar |🠋|--//
+    $('#resume-rightbar').css('display', 'flex');
+    setTimeout(() => {
+      $('#rightbar-portrait').removeClass('hidden');
+      $('#rightbar-portrait').addClass('visible');
+    }, 125);
+  }
+  function hideRightbar() {
+    //--|🠋| Hide Rightbar |🠋|--//
+    $('#rightbar-portrait').removeClass('visible');
+    $('#rightbar-portrait').addClass('hidden');
+    setTimeout(() => {
+      $('#resume-rightbar').css('display', 'none');
+    }, 375);
   }
 }
--- src/front-end/pages/resume/F-rightbar/default-rightbar/default-rightbar.ts
+++ src/front-end/pages/resume/F-rightbar/default-rightbar/default-rightbar.ts
@@ -2,27 +2,41 @@ export namespace DefaultRightbar {
   export function events() {
     //--🠋 Mobile (Portrait) 🠋--//
     $('#rightbar-navigation div a').on('click', (event) => {
-      //--|🠋| Downplay Header |🠋|--//
-      $('#rightbar-hitbox').removeClass('active');
-      $('#header-portrait').removeClass('active');
+      downplayHeader();
+      selectRightbar(event);
 
-      //--|🠋| Highlight Rightbar |🠋|--//
-      $('#rightbar-navigation div').removeClass('active');
-      $(event.currentTarget).addClass('active');
-
-      //--|🠋| Hide Rightbar |🠋|--//
-      $('#rightbar-portrait').removeClass('visible');
-      $('#rightbar-portrait').addClass('hidden');
-      setTimeout(() => {
-        $('#resume-rightbar').css('display', 'none');
-      }, 375);
+      hideRightbar();
     });
 
     $('#rightbar-navigation div').on('mouseover', (event) => {
-      //--|🠋| Scroll default-main |🠋|--//
-      window.location.href = event.currentTarget.children[0].getAttribute('href');
+      selectMain(event);
     });
 
     console.log('default-rightbar.js Detected!');
+  }
+
+  //--|🠋| default-rightbar |🠋|--//
+  function selectRightbar(event: any) {
+    //--|🠋| Remove all 'active' classes |🠋|--//
+    $('#rightbar-navigation div').removeClass('active');
+    //--|🠋| Highlight Clicked Rightbar |🠋|--//
+    $(event.currentTarget).addClass('active');
+  }
+  function selectMain(event: any) {
+    //--|🠋| Scroll default-main |🠋|--//
+    window.location.href = event.currentTarget.children[0].getAttribute('href');
+  }
+  function hideRightbar() {
+    //--|🠋| Hide Rightbar |🠋|--//
+    $('#rightbar-portrait').removeClass('visible');
+    $('#rightbar-portrait').addClass('hidden');
+    setTimeout(() => {
+      $('#resume-rightbar').css('display', 'none');
+    }, 375);
+  }
+  function downplayHeader() {
+    //--|🠋| Downplay Header |🠋|--//
+    $('#rightbar-hitbox').removeClass('active');
+    $('#header-portrait').removeClass('active');
   }
 }
