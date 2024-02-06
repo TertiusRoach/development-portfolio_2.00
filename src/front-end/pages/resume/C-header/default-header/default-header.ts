0dae31a9a27e936d4fc7396180faf52e4ca17530
Tertius Roach <tertius.roach@gmail.com>
2024-02-06 19:53:00 +0200

    Tweaked the User Experience: Test on Phone

--- src/front-end/pages/resume/C-header/default-header/default-header.ts
+++ src/front-end/pages/resume/C-header/default-header/default-header.ts
@@ -1,24 +1,52 @@
 export namespace DefaultHeader {
   export function events() {
     //--🠋 Desktop (Landscape) 🠋--//
-    $('#header-navigation a').on('click', function (event) {
+    $('#header-navigation a').on('click', (event) => {
       //--|🠋| Mark Header as Active |🠋|--//
       $('#header-navigation a').removeClass('active');
       $(event.currentTarget).addClass('active');
     });
 
     //--🠋 Mobile (Portrait) 🠋--//
-    $('#rightbar-hitbox').on('click', function () {
-      $('#resume-rightbar').css('display', 'flex');
+    $('#rightbar-hitbox')
+      .on('click', (event) => {
+        if (event.currentTarget.className === '') {
+          $('#resume-rightbar').css('display', 'flex');
 
-      setTimeout(() => {
-        $('#rightbar-portrait').removeClass('hidden');
-        $('#rightbar-portrait').addClass('visible');
+          setTimeout(() => {
+            $('#rightbar-hitbox').addClass('active');
+            $('#header-portrait').addClass('active');
 
-        $('#header-portrait').addClass('active');
-        $('#rightbar-hitbox').addClass('active');
-      }, 125);
-    });
+            //--|🠋| Show Rightbar |🠋|--//
+            $('#rightbar-portrait').removeClass('hidden');
+            $('#rightbar-portrait').addClass('visible');
+          }, 125);
+        }
+      })
+      .on('mouseover', (event) => {
+        if (event.currentTarget.className === '') {
+          $('#resume-rightbar').css('display', 'flex');
+
+          setTimeout(() => {
+            $('#rightbar-hitbox').addClass('active');
+            $('#header-portrait').addClass('active');
+
+            //--|🠋| Show Rightbar |🠋|--//
+            $('#rightbar-portrait').removeClass('hidden');
+            $('#rightbar-portrait').addClass('visible');
+          }, 125);
+        }
+      })
+      .on('mouseleave', (event) => {
+        // if (event.currentTarget.className === '') {
+        //   $('#rightbar-portrait').removeClass('visible');
+        //   $('#rightbar-portrait').addClass('hidden');
+        //   setTimeout(() => {
+        //     $('#resume-rightbar').css('display', 'none');
+        //   }, 375);
+        // }
+      });
+
     console.log('default-header.js Detected!');
   }
 }
--- src/front-end/pages/resume/F-rightbar/default-rightbar/default-rightbar.ts
+++ src/front-end/pages/resume/F-rightbar/default-rightbar/default-rightbar.ts
@@ -1,23 +1,34 @@
 export namespace DefaultRightbar {
   export function events() {
     //--🠋 Mobile (Portrait) 🠋--//
-    $('#rightbar-navigation div').on('click', function (event) {
-      //--|🠋| Highlight Button |🠋|--//
+    $('#rightbar-navigation div a').on('click', (event) => {
+      //--|🠋| Downplay Header |🠋|--//
+      $('#rightbar-hitbox').removeClass('active');
+      $('#header-portrait').removeClass('active');
+
+      //--|🠋| Highlight Rightbar |🠋|--//
       $('#rightbar-navigation div').removeClass('active');
       $(event.currentTarget).addClass('active');
-    });
 
-    $('#rightbar-portrait').on('click', function () {
       //--|🠋| Hide Rightbar |🠋|--//
       $('#rightbar-portrait').removeClass('visible');
       $('#rightbar-portrait').addClass('hidden');
-
-      $('#header-portrait').removeClass('active');
-      $('#rightbar-hitbox').removeClass('active');
-
       setTimeout(() => {
         $('#resume-rightbar').css('display', 'none');
-      }, 125);
+      }, 375);
+    });
+
+    $('#rightbar-portrait').on('mouseleave', () => {
+      //--|🠋| Downplay Header |🠋|--//
+      $('#rightbar-hitbox').removeClass('active');
+      $('#header-portrait').removeClass('active');
+
+      //--|🠋| Hide Rightbar |🠋|--//
+      $('#rightbar-portrait').removeClass('visible');
+      $('#rightbar-portrait').addClass('hidden');
+      setTimeout(() => {
+        $('#resume-rightbar').css('display', 'none');
+      }, 375);
     });
     console.log('default-rightbar.js Detected!');
   }
